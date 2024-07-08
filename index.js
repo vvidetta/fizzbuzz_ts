b388de26303854959ff56a749a71ec79f632a025
Vito Videtta <vvidetta@yahoo.co.uk>
2024-07-08 16:50:01 +0100

    Set up app framework

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+// Copyright (C) 2024 Vito Videtta. All rights reserved.
+
+const process = require('node:process');
+
+function main() {
+    return 1;
+}
+
+process.exit(main())
