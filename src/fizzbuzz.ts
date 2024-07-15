24b35e39301a73153cfd3c9a514bb533cdf92558
Vito Videtta <vvidetta@yahoo.co.uk>
2024-07-15 09:48:31 +0100

    fizzbuzz of 2 is 2

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,5 +1,5 @@
 // Copyright (C) 2024 Vito Videtta. All rights reserved.
 
 export function fizzbuzz(n: number): string {
-    return "1"
+    return n.toString();
 }
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -5,5 +5,8 @@ import { fizzbuzz } from '../src/fizzbuzz';
 describe("fizzbuzz tests", () =>{
   test("fizzbuzz of 1 is 1", () =>{
     expect(fizzbuzz(1)).toBe("1");
-  })
+  });
+  test("fizzbuzz of 2 is 2", () =>{
+    expect(fizzbuzz(2)).toBe("2");
+  });
 });
