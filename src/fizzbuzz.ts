d33c5d61adc7737c73299da0e2f7dc8caad97bd3
Vito Videtta <vvidetta@yahoo.co.uk>
2024-07-15 09:46:43 +0100

    refactoring

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,5 +1,5 @@
 // Copyright (C) 2024 Vito Videtta. All rights reserved.
 
-export function fb(n: number): string {
+export function fizzbuzz(n: number): string {
     return "1"
 }
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -1,9 +1,9 @@
 // Copyright (C) 2024 Vito Videtta. All rights reserved.
 
-import { fb } from '../src/fizzbuzz';
+import { fizzbuzz } from '../src/fizzbuzz';
 
 describe("fizzbuzz tests", () =>{
   test("fizzbuzz of 1 is 1", () =>{
-    expect(fb(1)).toBe("1");
+    expect(fizzbuzz(1)).toBe("1");
   })
 });
